a4736749b434f2bf9fa3f65e6692d52f04e26599
langalang <1019932358@qq.com>
2025-07-20 14:59:49 +0800

    feat(langhua-ai-agent)： 完整版

--- src/main/java/com/kailangye/langhuaaiagent/demo/invoke/TestApiKey.java
+++ src/main/java/com/kailangye/langhuaaiagent/demo/invoke/TestApiKey.java
@@ -2,5 +2,5 @@ package com.kailangye.langhuaaiagent.demo.invoke;
 
 public interface TestApiKey {
 
-    String API_KEY = "sk-59e50f67b8ec46578b227caa87e98019";
+    String API_KEY = "你自己的APIKey";
 }
